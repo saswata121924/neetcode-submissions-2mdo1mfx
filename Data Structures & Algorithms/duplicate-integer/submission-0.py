c0e73cb90373e6179a325fdd8cfa731a97499ce4
Saswata Deb <45950047+saswata121924@users.noreply.github.com>
2026-04-09 13:56:32 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        map = {}
+        for i in nums:
+            if i not in map.keys():
+                map[i] = 1
+            else:
+                return True
+        return False
